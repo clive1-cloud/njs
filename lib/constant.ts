58fdf2b8e39968f545cc3596d199b826c9a3aa28
mr1-5 <ogumecharlie@gmail.com>
2026-03-06 10:46:53 +0100

    added cart section

--- lib/constant.ts
+++ lib/constant.ts
@@ -5,3 +5,7 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An e-commerce platform that offers a wide range of products at competitive prices, ensuring a seamless shopping experience for all users.'
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+
+export const FREE_SHIPPING_MIN_PRICE = Number (
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+)
