711e2fab48e134ba36066af32f2905b8557a13e4
mr1-5 <ogumecharlie@gmail.com>
2026-05-21 00:45:18 +0100

    goood

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: false, // keep optimization on
+    localPatterns: [
+      {
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
